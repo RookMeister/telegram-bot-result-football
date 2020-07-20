633c1200da24c6d585ee53216aca5794ed1f8173
Artem Hmel <artemhmel6@gmail.com>
2020-07-20 17:21:23 +1000

    add actions

--- index.js
+++ index.js
@@ -10,33 +10,39 @@ const WizardScene = require('telegraf/scenes/wizard')
 // const stage = new Stage([superWizard], { default: 'super-wizard' })
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+const listCountry = ['russian', 'britan', 'spain', 'germany', 'france'];
+// const {}
+
 // API Турнирная таблица
 function returnApi(countryNumber, viewResult) {
   return `https://www.sports.ru/core/stat/gadget/${viewResult}/?args={%22tournament_id%22:${countryNumber}}`;
 }
 
+bot.use(session())
+
 bot.start((ctx) => {
   ctx.reply('Добро пожаловать. Выберите страну!', Markup.inlineKeyboard([
-    Markup.callbackButton('🇷🇺', 'russian')
+    Markup.callbackButton('🇷🇺', 31),
+    Markup.callbackButton('🏴󠁧󠁢󠁥󠁮󠁧󠁿', 52),
+    Markup.callbackButton('🇪🇸', 49),
+    Markup.callbackButton('🇮🇹', 48),
+    Markup.callbackButton('🇩🇪', 50),
+    Markup.callbackButton('🇫🇷', 51),
   ]).extra())
 })
-bot.action('russian', (ctx) => {
-  console.log(1, ctx.match);
-  // numberCountry = 31;
+
+bot.action(/^[0-9]{2}$/i, ctx => {
+  ctx.session.country = ctx.update.callback_query.data || null;
   ctx.reply('Выберите информацию', Markup.inlineKeyboard([
     Markup.callbackButton('Турнирная таблица', 'tournament_table')
   ]).extra())
 })
-bot.on('callback_query', async (ctx) => {
-  console.log(2, ctx.update.callback_query.data)
-  const url = returnApi(31, ctx.update.callback_query.data);
-  console.log(3, url);
-  const info = await getData(url);
-  console.log(info)
-  ctx.replyWithMarkdown(info)
-})
 
-bot.use(session())
+bot.on('callback_query', async (ctx) => {
+  const url = returnApi(ctx.session.country, ctx.update.callback_query.data);
+  const info = await getData(url);
+  ctx.replyWithHTML(info)
+})
 
 bot.launch()
 
@@ -45,13 +51,13 @@ async function getData(url) {
   try {
     const data = await fetch(url);
     const json = await data.json();
-    
-    const team = json.tournament_table[0].list[0];
-    console.log(json.tournament_table[0].list[0])
-    // const author = json.quoteAuthor.length === 0 ? 'Автор не известен' : json.quoteAuthor;
-    return `*${team.place} место* \u2014 _команда ${team.team_info.name}_`;
+    let result = '';
+    json.tournament_table[0].list.forEach(element => {
+      result +=`<b>${element.place} место</b> \u2014 <i>${element.team_info.name}</i>\r\n`;
+    });
+    return result;
   } catch (err) {
     console.error('Fail to fetch data: ' + err);
-    // return 'Мысль потеряна! Попробуй ещё раз.';
+    return 'Ошибка';
   }
 }
