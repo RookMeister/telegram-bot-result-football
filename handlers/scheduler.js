a870c56b27e62399680a20ffd76308546d31283f
Artem Hmel <artemhmel6@gmail.com>
2020-11-24 08:20:25 +1000

    fix

--- handlers/scheduler.js
+++ handlers/scheduler.js
@@ -3,19 +3,18 @@ const { dataConversionChampionat } = require('../utils/helpers')
 const User = require('../models/user');
 
 async function startScheduler(bot) {
-  let isSend = false;
+  
   setInterval(async () => {
+    let isSend = false;
     const users = await User.find({});
-    if (users.length) {
+    if (users.length && !isSend) {
       users.forEach(async (el) => {
         const data = await getData('championat', { date: 'now', subscriptions: el.subscriptions, check: true });
         let info = dataConversionChampionat(data);
         info = (info === 'Нет подходящих матчей') ? '' : info;
-        if (info && !isSend) {
+        if (info && el.onScheduler) {
           sendMessage(bot.telegram, el.chat_id, info);
           isSend = true;
-        } else if (!info) {
-          isSend = false;
         }
       });
     }
