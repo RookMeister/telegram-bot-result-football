d1f3bba4d87cf4d084563033375649bb0d1b84d1
Artem Hmel <artemhmel6@gmail.com>
2020-11-24 15:55:56 +1000

    fix

--- handlers/timeZone.js
+++ handlers/timeZone.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 
 function setupTimeZone(bot) {
   bot.hears('Настроить время', (ctx) => showTomeZone(ctx));
-  bot.action(new RegExp(timeZone.map(el => '\\'+el).join("|")), (ctx) => setTimeZone(ctx));
+  bot.action(new RegExp(timeZone.map(el => '\\'+el).join("|")), async (ctx) => setTimeZone(ctx));
 }
 
 function showTomeZone(ctx) {
@@ -19,10 +19,10 @@ async function setTimeZone(ctx) {
   await User.findOneAndUpdate({chat_id: ctx.chat.id}, { $set: { timeZone: ctx.match.input } }, function (err, data) {
     if (err) return console.log(err);
     ctx.session.user = data;
+    const date = addHours(new Date(), Number(ctx.session.user.timeZone))
+    const info = `Твой часовой пояс ${ctx.session.user.timeZone}. Так что теперь время ${format(date, 'HH:mm')}. Если это не так, измени часовой пояс:`;
+    ctx.replyWithHTML(info);
   });
-  const date = addHours(new Date(), Number(ctx.session.user.timeZone))
-  const info = `Твой часовой пояс ${ctx.session.user.timeZone}. Так что теперь время ${format(date, 'HH:mm')}. Если это не так, измени часовой пояс:`;
-  ctx.replyWithHTML(info);
 }
 
 
