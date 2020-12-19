44f0deb7f72bf772ce54e3592e51d8170404d907
Artem Hmel <artemhmel6@gmail.com>
2020-12-19 02:26:22 +0000

    fix

--- handlers/scheduler.js
+++ handlers/scheduler.js
@@ -2,33 +2,37 @@ const { getData } = require('../helpers/api')
 const { getDataMatches, conversionDataMatches, getInfoForLike } = require('../helpers/matches')
 const { findAllUsers } = require('../models/user');
 
-let isSend = false;
-let isSendClubs = false;
+
+
 
 const timeoutPromise = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
 
 async function startScheduler(bot) {
+  const users = await findAllUsers();
+  const isSend = Array(users.length).fill(false);
+  const isSendClubs = Array(users.length).fill(false);
+
   setInterval(async () => {
     const users = await findAllUsers();
     if (users.length) {
-      for (const el of users) {
+      for (let [i, el] of users.entries()) {
         if (el.onScheduler || el.likeClub.length) {
           const json = await getData('matches', { date: 'now' });
           if (el.onScheduler) {
             const info = getMatches({json, subscriptions: el.subscribeTournaments, timeZone: Number(el.timeZone), checkEnd: true});
             info && !isSend && await sendMessage(bot.telegram, el.chat_id, info);
-            isSend = (info) ? true : false;
+            isSend[i] = (info) ? true : false;
           }
           if (el.likeClub.length) {
             const info = getInfoForLike({data: json, likeClubs: el.likeClub, timeZone: Number(el.timeZone)})
             const condition = info && info.includes('10');
-            condition && !isSendClubs && await sendMessage(bot.telegram, el.chat_id, info);
-            isSendClubs = (condition) ? true : false;
+            condition && !isSendClubs[i] && await sendMessage(bot.telegram, el.chat_id, info);
+            isSendClubs[i] = (condition) ? true : false;
           }
         }
       };
     }
-  }, 60000);
+  }, 6000);
 }
 
 function getMatches({json, subscriptions, timeZone, checkEnd}) {
@@ -38,6 +42,7 @@ function getMatches({json, subscriptions, timeZone, checkEnd}) {
 }
 
 async function sendMessage(ctx, chatId, info) {
+  console.log(1);
   try {
     await timeoutPromise(300);
     console.log('sendMessage',!!info, chatId);
