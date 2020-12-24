e48917ed696744ef7e3c1b285a6b105f3bbffe14
Artem Hmel <artemhmel6@gmail.com>
2020-12-25 09:12:47 +1000

    fix

--- handlers/scheduler.js
+++ handlers/scheduler.js
@@ -19,7 +19,7 @@ async function startScheduler(bot) {
         if (el.onScheduler || el.likeClub.length) {
           const json = await getData('matches', { date: 'now' });
           if (el.onScheduler) {
-            const info = getMatches({data: json, subscriptions: el.subscribeTournaments, timeZone: Number(el.timeZone), checkEnd: true});
+            const info = getMatches({json, subscriptions: el.subscribeTournaments, timeZone: Number(el.timeZone), checkEnd: true});
             info && !isSend[i] && await sendMessage(bot.telegram, el.chat_id, info);
             isSend[i] = (info) ? true : false;
           }
